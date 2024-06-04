495680fb1b4c111097f11299b16b92928fb2dede
Udit gupta <udit81067@gmail.com>
2024-06-04 08:35:26 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function myMenuFunction() {
+    var menuBth = document.getElementById("myNavMenu");
+
+    if (menuBth.className === "nav-menu"){
+        menuBth.className += "responsive"
+    } else{
+        menuBth.className = "nav-menu";
+    }
+
+}
